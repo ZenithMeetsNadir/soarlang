ba5ef6ce25d1a00adaaf34970614387d3913ca7b
ZenithMeetsNadir <159653031+ZenithMeetsNadir@users.noreply.github.com>
2024-11-07 19:56:38 +0100

    addition of a very useful command (#1)
    * hmmm what could this be...
    
    * update commandlist
    
    * rename dergcommand to dergcommand.zig

--- src/lib/commandline/commandlist.zig
+++ src/lib/commandline/commandlist.zig
@@ -1,6 +1,8 @@
 const ICommand = @import("ICommand.zig");
 const help_command = @import("commands/helpcommand.zig").help_command;
+const derg_command = @import("commands/dergcommand.zig").derg_command;
 
 pub const command_list = [_]ICommand{
     help_command,
+    derg_command,
 };
